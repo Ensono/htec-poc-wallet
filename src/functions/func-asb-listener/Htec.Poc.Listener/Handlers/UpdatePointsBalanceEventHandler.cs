96eeccdfad97a8b9acd7d7d241d378c6d4d66a12
scottvaughanamido <scott.vaughan@amido.com>
2022-05-06 10:02:21 +0100

    Add exception handling

--- src/functions/func-asb-listener/Htec.Poc.Listener/Handlers/UpdatePointsBalanceEventHandler.cs
+++ src/functions/func-asb-listener/Htec.Poc.Listener/Handlers/UpdatePointsBalanceEventHandler.cs
@@ -2,47 +2,48 @@
 using Amido.Stacks.Data.Documents.Abstractions;
 using Htec.Poc.Application.CQRS.Events;
 using Htec.Poc.Application.Integration;
+using Htec.Poc.Common.Exceptions;
 using Microsoft.Extensions.Logging;
-using System;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Htec.Poc.Listener.Handlers
-{
-    public class UpdatePointsBalanceEventHandler : IApplicationEventHandler<RewardCalculatedEvent>
-    {
-        private readonly IWalletRepository repository;
-        private readonly ILogger<UpdatePointsBalanceEventHandler> logger;
-        readonly IDocumentSearch<Domain.Wallet> storage;
+namespace Htec.Poc.Listener.Handlers;
 
-        public UpdatePointsBalanceEventHandler(ILogger<UpdatePointsBalanceEventHandler> logger, IWalletRepository repository, IDocumentSearch<Domain.Wallet> storage)
+public class UpdatePointsBalanceEventHandler : IApplicationEventHandler<RewardCalculatedEvent>
+{
+    private readonly IWalletRepository repository;
+    private readonly ILogger<UpdatePointsBalanceEventHandler> logger;
+    readonly IDocumentSearch<Domain.Wallet> storage;
+
+    public UpdatePointsBalanceEventHandler(ILogger<UpdatePointsBalanceEventHandler> logger, IWalletRepository repository, IDocumentSearch<Domain.Wallet> storage)
+    {
+        this.logger = logger;
+        this.repository = repository;
+        this.storage = storage;
+    }
+
+    public async Task HandleAsync(RewardCalculatedEvent applicationEvent)
+    {
+        logger.LogInformation($"Executing UpdatePointsBalanceEventHandler...");
+
+        var results = await storage.Search(filter => filter.MemberId == applicationEvent.MemberId);
+        if (!results.IsSuccessful)
         {
-            this.logger = logger;
-            this.repository = repository;
-            this.storage = storage;
+            OperationFailedException.Raise(applicationEvent, applicationEvent.MemberId, "Unable to find wallet");
+        }
+           
+        var wallet = results.Content.FirstOrDefault();
+        if (wallet == null)
+        {
+            MemberDoesNotExistException.Raise(applicationEvent, applicationEvent.MemberId);
         }
 
-        public async Task HandleAsync(RewardCalculatedEvent applicationEvent)
+        wallet.UpdatePointsBalance(applicationEvent.Points);
+
+        var successful = await repository.SaveAsync(wallet);
+        if (!successful)
         {
-            logger.LogInformation($"Executing UpdatePointsBalanceEventHandler...");
-
-            var results = await storage.Search(filter => filter.MemberId == applicationEvent.MemberId);
-            if (!results.IsSuccessful)
-            {
-                logger.LogInformation($"Wallet not found in database.");
-                throw new Exception("NOT FOUND!");
-            }
-               
-            var wallet = results.Content.FirstOrDefault();
-
-            wallet.UpdatePointsBalance(applicationEvent.Points);
-
-            var successful = await repository.SaveAsync(wallet);
-            if (!successful)
-            {
-                logger.LogInformation($"Wallet failed to save successfully.");
-                throw new Exception("SAVE FAILED!");
-            }
+            OperationFailedException.Raise(applicationEvent, applicationEvent.MemberId, "Unable to update wallet");
         }
     }
 }
